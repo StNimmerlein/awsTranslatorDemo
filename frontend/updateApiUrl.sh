35c41c13e8be5b26979882716023b2076ab2ac7c
Marco Sieben <16012551+StNimmerlein@users.noreply.github.com>
2019-11-11 20:47:08 +0100

    Add deployment

--- frontend/updateApiUrl.sh
+++ frontend/updateApiUrl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-TEMPLATE_ENV_LOCATION='../frontend/src/environments/environment.prod.template.ts'
-PROD_ENV_LOCATION='../frontend/src/environments/environment.prod.ts';
+TEMPLATE_ENV_LOCATION='./src/environments/environment.prod.template.ts'
+PROD_ENV_LOCATION='./src/environments/environment.prod.ts';
 
 apiUrl=$(aws cloudformation describe-stacks --stack-name InfrastructureStack --query "Stacks[0].Outputs[?OutputKey=='ApiOutput'].OutputValue" --output text)
 
