891bd1ce05fd3764888ba2bf6e04abd092d00c46
Marco Sieben <16012551+StNimmerlein@users.noreply.github.com>
2019-11-11 19:47:57 +0100

    Enable CORS

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -3,14 +3,14 @@ import lambda = require('@aws-cdk/aws-lambda');
 import iam = require('@aws-cdk/aws-iam');
 import s3 = require('@aws-cdk/aws-s3');
 import apigw = require('@aws-cdk/aws-apigateway');
-
-
 import path = require('path');
 import { Runtime } from '@aws-cdk/aws-lambda';
 import { Duration } from '@aws-cdk/core';
 import { PassthroughBehavior } from '@aws-cdk/aws-apigateway';
 
 export class InfrastructureStack extends cdk.Stack {
+  private jarLocation = '../../lambda/build/libs/translation-test-1.0-SNAPSHOT-all.jar';
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -28,7 +28,7 @@ export class InfrastructureStack extends cdk.Stack {
     let translateLambda = new lambda.Function(this, 'translateText', {
       runtime: Runtime.JAVA_8,
       handler: 'de.marco_sieben.App::handleRequest',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda/build/libs/translation-test-1.0-SNAPSHOT-all.jar')),
+      code: lambda.Code.fromAsset(path.join(__dirname, this.jarLocation)),
       timeout: Duration.seconds(15),
       memorySize: 256,
       role: translateRole,
@@ -43,18 +43,32 @@ export class InfrastructureStack extends cdk.Stack {
       proxy: false,
       passthroughBehavior: PassthroughBehavior.WHEN_NO_TEMPLATES,
       integrationResponses: [{
-        statusCode: '200'
+        statusCode: '200',
+        responseParameters: {
+          'method.response.header.Access-Control-Allow-Headers': '\'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent\'',
+          'method.response.header.Access-Control-Allow-Origin': '\'*\'',
+          'method.response.header.Access-Control-Allow-Methods': '\'GET\''
+        }
       }],
       requestTemplates: {
-        'application/json': JSON.stringify({
-          text: '$util.escapeJavaScript($input.params(\'text\'))',
-          targetLanguageCode: '$input.params(\'language\')'
-        })
+        'application/json': `{
+          "text": "$util.escapeJavaScript($input.params('text')).replaceAll("\\\\'", "'")",
+          "targetLanguageCode": "$input.params('language')"
+        }`
       }
+    });
+    langResource.addCorsPreflight({
+      allowMethods: ['OPTIONS', 'GET'],
+      allowOrigins: ['*']
     });
     langResource.addMethod('GET', getIntegration, {
       methodResponses: [{
-        statusCode: '200'
+        statusCode: '200',
+        responseParameters: {
+          'method.response.header.Access-Control-Allow-Headers': true,
+          'method.response.header.Access-Control-Allow-Origin': true,
+          'method.response.header.Access-Control-Allow-Methods': true
+        }
       }]
     });
   }
